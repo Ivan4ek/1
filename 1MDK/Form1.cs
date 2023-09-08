90d04732e05a610f5583f9c2b0c43872c5d2a7d3
Ivan4ek <128650625+Ivan4ek@users.noreply.github.com>
2023-09-09 01:39:29 +0400

    Add files via upload

---
+++ 1MDK/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImg1_Click(object sender, EventArgs e)
+        {
+            // Полный путь к изображению с расширением файла (например, .jpg, .png)
+            string imagePath = @"C:\GitProjectMDK\1MDK\1MDK\bin\Debug\img1.jpg";
+
+            try
+            {
+                // Освобождаем ресурсы, связанные с предыдущим изображением (если оно было загружено)
+                if (pictureBox1.Image != null)
+                {
+                    pictureBox1.Image.Dispose();
+                }
+
+                // Устанавливаем режим отображения изображения по центру PictureBox
+                pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                // Отобразить изображение в PictureBox
+                pictureBox1.Image = new Bitmap(imagePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки изображения: {ex.Message}");
+            }
+        }
+    }
+}
